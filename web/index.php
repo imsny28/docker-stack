025b1dd454e62774c722447675560f9478163501
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-12-11 01:57:48 +0530

    working rails on docker compose

--- web/index.php
+++ web/index.php
@@ -1,7 +1,28 @@
 <?php
 echo 'Docker-compose example. If you see this, it means you have not mounted any web content on the default root.';
 
+// $db_connection = pg_connect("host=localhost port = 5432 dbname=rails_app_development user=postgres password=Free123!");
+// $db_connection = pg_connect('dbname=rails_app_development host=demo_postgres_1 port=5432 user=postgres password=Free123!');
+// $dbconn = pg_connect("host=demo_postgres_1 port=5432 dbname=rails_app_development user=postgres password=Free123!")or die("Could not connect");
 
+// Connect to the database
+$dbconn = pg_connect("host=postgres dbname=rails_app_development user=postgres password=Free123!");
+// Show the client and server versions
+// print_r(pg_version($dbconn));
+
+// print_r($db_connection);
+
+// $host        = "host = 127.0.0.1";
+//  $port        = "port = 5432";
+//  $dbname      = "dbname = rails_app_development";
+//  $credentials = "user = postgres password=Free123!";
+//
+//  $db = pg_connect( "$host $port $dbname $credentials"  );
+//  if(!$db) {
+//     echo "Error : Unable to open database\n";
+//  } else {
+//     echo "Opened database successfully\n";
+//  }
 // $servername = "db";
 // $username = "root";
 // $password = "maro123#";
